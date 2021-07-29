36b7065a9553fb485d0ed9af6ebe386c1a09f833
Ivo <81589883+Ivo27@users.noreply.github.com>
2021-07-29 18:51:51 -0300

    Add files via upload

---
+++ Business.Entities/Especialidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Especialidad: BusinessEntity
+    {
+        private string _descEspecialidad;
+
+        public string DescEspecialidad { get { return _descEspecialidad; } set { _descEspecialidad = value; }}
+    }
+}
