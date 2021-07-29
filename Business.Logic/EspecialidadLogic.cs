793e3a5b71db33c3019b3357529478cc9a9663da
Ivo <81589883+Ivo27@users.noreply.github.com>
2021-07-29 18:53:06 -0300

    Add files via upload

---
+++ Business.Logic/EspecialidadLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    public class EspecialidadLogic: BusinessLogic
+    {
+        private EspecialidadAdapter _especialidadData;
+
+        public EspecialidadAdapter EspecialidadData { get { return _especialidadData; } set { _especialidadData = value; } }
+
+        public EspecialidadLogic()
+        {
+            _especialidadData = new EspecialidadAdapter();
+        }
+
+        public List<Especialidad> GetAll()
+        {
+            return _especialidadData.GetAll();
+        }
+
+        public Especialidad GetOne(int idEspecialidad)
+        {
+            return _especialidadData.GetOne(idEspecialidad);
+        }
+
+        public void Delete(int idEspecialidad)
+        {
+            _especialidadData.Delete(idEspecialidad);
+        }
+
+        public void Save(Especialidad especialidad)
+        {
+            _especialidadData.Save(especialidad);
+        }
+
+    }
+}
---
+++ Business.Logic/PersonaLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    class PersonaLogic:BusinessLogic
+    {
+        private PersonaAdapter _personaData;
+
+        public PersonaAdapter PersonaData { get { return _personaData; } set { _personaData = value; } }
+
+        public PersonaLogic()
+        {
+            _personaData = new PersonaAdapter();
+        }
+
+
+        public List<Persona> GetAll()
+        {
+            return _personaData.GetAll();
+        }
+
+        public Persona GetOne(int idPersona)
+        {
+            return _personaData.GetOne(idPersona);
+        }
+
+        public void Delete(int idPersona)
+        {
+            _personaData.Delete(idPersona);
+        }
+
+        public void Save(Persona persona)
+        {
+            _personaData.Save(persona);
+        }
+    }
+}
