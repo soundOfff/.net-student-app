2199d1b18f16602051b1a790ea3d26bbc9525df3
tomibrasca <tomibrasca97@gmail.com>
2021-08-02 16:41:21 -0300

    Agregue ABMS

--- Business.Logic/Validaciones.cs
+++ Business.Logic/Validaciones.cs
@@ -9,11 +9,54 @@ namespace Business.Logic
 {
     public class Validaciones
     {
-        public static bool EsMailValido(string mail)
+        /*public static bool EsMailValido(string mail)
         {
             String expresion = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
             return Regex.IsMatch(mail, expresion);
         }
+        */
+        public static bool IsValidEmail(string email)
+        { 
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                // Se puede sacar
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new System.Globalization.IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
 
     }
 }
---
+++ Data.Database/ComisionAdapter.cs
@@ -0,0 +1,163 @@
+﻿using Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Data.Database
+{
+    public class ComisionAdapter : Adapter
+    {
+        public List<Comision> GetAll()
+        {
+            List<Comision> comisiones = new List<Comision>();
+
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdComisiones = new SqlCommand("SELECT * FROM comisiones", SqlConn);
+                SqlDataReader drComisiones = cmdComisiones.ExecuteReader();
+                while (drComisiones.Read())
+                {
+                    Comision com = new Comision();
+                    com.ID = (int)drComisiones["id_comision"];
+                    com.DescComision = (string)drComisiones["desc_comision"];
+                    com.AnioEspecialidad = (int)drComisiones["anio_especialidad"];
+                    // Foregin key ?
+                    comisiones.Add(com);
+                }
+                drComisiones.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar la lista de Comisiones", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return comisiones;
+        }
+
+        public Business.Entities.Comision GetOne(int ID)
+        {
+            Comision com = new Comision();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdComisiones = new SqlCommand("select * from comisiones where id_comision = @id", SqlConn);
+                cmdComisiones.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                SqlDataReader drComisiones = cmdComisiones.ExecuteReader();
+                if (drComisiones.Read())
+                {
+                    com.ID = (int)drComisiones["id_comision"];
+                    com.DescComision = (string)drComisiones["desc_comision"];
+                    com.AnioEspecialidad = (int)drComisiones["anio_especialidad"];
+                }
+                drComisiones.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar datos de la comision", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return com;
+        }
+
+        public void Delete(int ID)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdDelete = new SqlCommand("delete comisiones where id_comision = @id", SqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al elminiar la comision", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Save(Comision comision)
+        {
+            if (comision.State == BusinessEntity.States.New)
+            {
+                this.Insert(comision);
+            }
+            else if (comision.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(comision.ID);
+            }
+            else if (comision.State == BusinessEntity.States.Modified)
+            {
+                this.Update(comision);
+            }
+            comision.State = BusinessEntity.States.Unmodified;
+        }
+        protected void Update(Comision comision)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand(
+                    "UPDATE comisiones SET desc_comision = @desc_comision," +
+                    " anio_especialidad = @anio_especialidad" +
+                    " WHERE id_comision = @id", SqlConn);
+
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = comision.ID;
+                cmdSave.Parameters.Add("@desc_comision", SqlDbType.VarChar, 50).Value = comision.DescComision;
+                cmdSave.Parameters.Add("@anio_especialidad", SqlDbType.Int).Value = comision.AnioEspecialidad;
+                // Ver foreign keys
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al modificar datos de la comision", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        protected void Insert(Comision comision)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand(
+                    "insert into comisiones(desc_comision, anio_especialidad) " +
+                    "values(@desc_comision, @anio_especialidad) " +
+                    "select @@identity", SqlConn);
+                // Esta linea es para recuperar el id que asigno el sql automaticamente
+
+                cmdSave.Parameters.Add("@desc_comision", SqlDbType.VarChar, 50).Value = comision.DescComision;
+                cmdSave.Parameters.Add("@anio_especialidad", SqlDbType.Int).Value = comision.AnioEspecialidad;
+                comision.ID = Decimal.ToInt32((decimal)cmdSave.ExecuteScalar());
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al crear la comision", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+
+    }
+}
